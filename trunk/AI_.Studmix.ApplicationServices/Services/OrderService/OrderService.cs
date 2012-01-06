f5b7c336712aa76de19abd2f03ab009a2d7fcda5
AI_ <AI_@61447f47-ac5e-44ff-bf46-c90db18f3561>
2012-01-06 17:00:45 +0000

    Заказ. -- Просмотр заказа на приобретение пакета.

---
+++ trunk/AI_.Studmix.ApplicationServices/Services/OrderService/OrderService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using AI_.Studmix.ApplicationServices.Services.OrderService.Requests;
+using AI_.Studmix.ApplicationServices.Services.OrderService.Responses;
+using AI_.Studmix.ApplicationServices.Specifications;
+using AI_.Studmix.Domain.Entities;
+using AI_.Studmix.Domain.Repository;
+using AI_.Studmix.Domain.Services.Abstractions;
+
+namespace AI_.Studmix.ApplicationServices.Services.OrderService
+{
+    public class OrderService : IOrderService
+    {
+        protected IUnitOfWork UnitOfWork { get; set; }
+        protected IFinanceService FinanceService { get; set; }
+
+        public OrderService(IUnitOfWork unitOfWork, IFinanceService financeService)
+        {
+            UnitOfWork = unitOfWork;
+            FinanceService = financeService;
+        }
+
+        public ViewOrderResponse ViewOrder(ViewOrderRequest request)
+        {
+            var response = new ViewOrderResponse();
+
+            var package = UnitOfWork.GetRepository<ContentPackage>().GetByID(request.PackageID);
+            var user = UnitOfWork.GetRepository<User>().Get(new GetUserByUserName(request.UserName)).Single();
+            new Order(user, package);
+
+            response.IsUserCanBuyPackage = FinanceService.UserCanBuyPackage(user, package);
+            response.UserBalance = user.Balance;
+            response.OrderPrice = package.Price;
+            return response;
+        }
+    }
+}
