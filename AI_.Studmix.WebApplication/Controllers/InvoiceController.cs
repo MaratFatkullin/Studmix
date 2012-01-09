b95a495f7f60b8d530f55ffd2359fb272e21500f
AI_ <AI_@61447f47-ac5e-44ff-bf46-c90db18f3561>
2012-01-09 15:31:49 +0000

    Личный кабинет. -- Выставление счета на пополнение баланса (только front-end).

---
+++ AI_.Studmix.WebApplication/Controllers/InvoiceController.cs
@@ -0,0 +1,37 @@
+﻿using System.Web.Mvc;
+using AI_.Studmix.ApplicationServices.Services.InvoiceService;
+using AI_.Studmix.ApplicationServices.Services.InvoiceService.Requests;
+using AI_.Studmix.WebApplication.ViewModels.Invoice;
+using AI_.Studmix.WebApplication.ViewModels.Shared;
+
+namespace AI_.Studmix.WebApplication.Controllers
+{
+    [Authorize]
+    public class InvoiceController : ControllerBase
+    {
+        public IInvoiceService InvoiceService { get; set; }
+
+        public InvoiceController(IInvoiceService invoiceService)
+        {
+            InvoiceService = invoiceService;
+        }
+
+        [HttpGet]
+        public ActionResult ReplenishBalance()
+        {
+            return View(new ReplenishBalanceViewModel());
+        }
+
+        [HttpPost]
+        public ActionResult ReplenishBalance(ReplenishBalanceViewModel viewModel)
+        {
+            var request = new ReplenishBalanceRequest {Amount = viewModel.Amount};
+            InvoiceService.ReplenishBalance(request);
+
+            return InformationView("Счет на пополнение баланса выставлен",
+                                   "На Ваш Qiwi-аккаунт выставлен счет к оплате.\n" +
+                                   "После оплаты счета Ваш балнс на STUDMIX будет пополнен на указаную сумму.",
+                                   new ActionLinkInfo("Account", "ViewAccount", "Вернуться в личный кабинет"));
+        }
+    }
+}
