92011ef5c908c7780c826208587d711461713320
AI_ <AI_@61447f47-ac5e-44ff-bf46-c90db18f3561>
2012-01-09 16:29:18 +0000

    Merge. -- /trunk:31-33,37 into qiwi_via_xml.

---
+++ AI_.Studmix.WebApplication/ViewModels/Account/UserDetailsViewModel.cs
@@ -0,0 +1,9 @@
+﻿using AI_.Studmix.ApplicationServices.DataTransferObjects;
+
+namespace AI_.Studmix.WebApplication.ViewModels.Account
+{
+    public class ViewAccountViewModel
+    {
+        public UserDto User { get; set; }
+    }
+}
